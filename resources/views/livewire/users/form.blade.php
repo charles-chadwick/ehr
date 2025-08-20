c866258ed9738af2b4639869e76a098aa33f7ce2
charles <charles@mutuallyassured.com>
2025-08-20 13:45:06 -0700

    Got file upload working for Avatar.
    Need to abstract this further, like putting the onFileUploaded and removeImage functions into a trait. I'm not sure where that trait should live, however.

--- resources/views/livewire/users/form.blade.php
+++ resources/views/livewire/users/form.blade.php
@@ -8,8 +8,10 @@ use Livewire\Volt\Component;
 use Flux\Flux;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
+use Illuminate\Support\Facades\DB;
 
 new class extends Component {
+
     public ?User  $user;
     public string $role                  = "";
     public string $prefix                = "";
@@ -21,11 +23,15 @@ new class extends Component {
     public string $password_confirmation = "";
     public string $modal                 = "";
 
+    public string $avatar_path = "";
+    public string $avatar_url  = "";
+
     public function mount(?User $user) : void
     {
         $this->user = $user;
         if ($this->user?->id) {
-            $this->fill($user);
+            $this->loadUser($this->user->id);
+
         }
     }
 
@@ -34,6 +40,25 @@ new class extends Component {
     {
         $this->user = User::findOrFail($id);
         $this->fill($this->user);
+        $this->avatar_url = $this->user->avatar;
+    }
+
+    #[On('file-uploaded')]
+    public function onFileUploaded(string $path, ?string $url = null) : void
+    {
+        $this->avatar_path = $path;
+        $this->avatar_url = $url ?? '';
+    }
+
+    public function removeImage() : void
+    {
+        DB::table('media')
+          ->where('model_type', User::class)
+          ->where('model_id', $this->user->id)
+          ->delete();
+        $this->avatar_url = "";
+        $this->avatar_path = "";
+
     }
 
     public function save() : void
@@ -65,6 +90,18 @@ new class extends Component {
             $heading = "User created";
         }
 
+        if ($this->avatar_path !== "") {
+            try {
+                $this->user->addMedia(storage_path('app/public/'.$this->avatar_path))
+                           ->preservingOriginal()
+                           ->toMediaCollection('avatars');
+            } catch (FileDoesNotExist|FileIsTooBig $e) {
+                dd($e->getMessage());
+                Flux::toast("Error saving avatar", heading: "Error", variant: "error", position: "top-right");
+
+            }
+        }
+
         // toast it up
         Flux::toast($message, heading: $heading, variant: "success", position: "top-right");
         $this->cancel();
@@ -92,7 +129,13 @@ new class extends Component {
             'password'              => $this->user->id ? 'nullable' : 'required'.'|string|min:1|max:255|confirmed',
             'password_confirmation' => $this->user->id ? 'nullable' : 'required'.'|string|min:1|max:255'
         ];
+    }
 
+    public function with() : array
+    {
+        return [
+            'user' => $this->user
+        ];
     }
 
 }; ?>
@@ -171,6 +214,40 @@ new class extends Component {
             />
         </div>
     </div>
+
+    <div
+            id="avatar"
+    >
+        <div class="mt-4">
+
+            @if($avatar_url)
+                <flux:label for="avatar">Avatar</flux:label>
+
+                <flux:avatar
+                        class="flex-none w-24 h-24  rounded-full object-cover mr-4"
+                        src="{{ $avatar_url }}"
+                        alt="{{ $user->full_name_extended }}"
+                        title="{{ $user->full_name_extended }}"
+                        size="md"
+                />
+                <a
+                        wire:click="removeImage"
+                        href="#"
+                        class="text-xs text-gray-500 truncate max-w-xs"
+                >Remove</a>
+            @else
+                <livewire:documents.upload
+                        directory="avatars"
+                        disk="public"
+                        event="file-uploaded"
+                        accept="image/*"
+                        label="Avatar"
+                        wire:key="avatar-uploader"
+                />
+            @endif
+        </div>
+    </div>
+
 
     <div class="flex items-center justify-center gap-4 py-4">
         <flux:button
