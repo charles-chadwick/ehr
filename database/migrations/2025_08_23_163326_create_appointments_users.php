2fe50337ef331832fb3f9f4f0bce3913ba76112f
charles <charles@mutuallyassured.com>
2025-08-23 11:21:35 -0700

    Added primary key

--- database/migrations/2025_08_23_163326_create_appointments_users.php
+++ database/migrations/2025_08_23_163326_create_appointments_users.php
@@ -11,6 +11,7 @@ return new class extends Migration {
     public function up() : void
     {
         Schema::create('appointments_users', function (Blueprint $table) {
+            $table->id();
             $table->foreignIdFor(Appointment::class);
             $table->foreignIdFor(User::class);
             $table->timestamps();
