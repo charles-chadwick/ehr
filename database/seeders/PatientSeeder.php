9e2c5204b0889028f85f7f4fb01c1a6b6380aad1
charles <charles@mutuallyassured.com>
2025-08-24 17:33:38 -0700

    Fixing status

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 
+use App\Enums\PatientStatus;
 use App\Models\Patient;
 use App\Models\User;
 use GuzzleHttp\Client;
@@ -21,7 +22,28 @@ class PatientSeeder extends Seeder
     {
         Patient::truncate();
         $characters = collect(json_decode(file_get_contents(database_path('src/rickandmorty_characters.json')),
-            true))->whereNotIn('id', [3,5,9,10,23,29,41,59,108,109,111,121,139,146,149,163,187,196,218,21]);
+            true))->whereNotIn('id', [
+            3,
+            5,
+            9,
+            10,
+            23,
+            29,
+            41,
+            59,
+            108,
+            109,
+            111,
+            121,
+            139,
+            146,
+            149,
+            163,
+            187,
+            196,
+            218,
+            21
+        ])->random(300);
         $already_in = [];
         $counter = 0;
 
@@ -36,12 +58,19 @@ class PatientSeeder extends Seeder
                 continue;
             }
 
-            $admin = User::where('role', '!=', 'Administrator')->inRandomOrder()->first();
+            $admin = User::where('role', '!=', 'Administrator')
+                         ->inRandomOrder()
+                         ->first();
 
             $first_name = array_shift($name);
             $last_name = array_pop($name);
 
-            if (in_array($first_name, ['Mrs', 'Ms', 'Mr'])) {
+            if (in_array($first_name, [
+                'Mrs',
+                'Ms',
+                'Mr'
+            ])
+            ) {
                 $first_name = array_shift($name);
             }
 
@@ -49,26 +78,42 @@ class PatientSeeder extends Seeder
 
             $created_at = fake()->dateTimeBetween($admin->created_at, '-1 year');
             $prefix = match ($character['gender']) {
-                'Male' => 'Mr',
-                'Female' => fake()->randomElement(['Mrs', 'Ms', '']),
-                default => '',
+                'Male'   => 'Mr',
+                'Female' => fake()->randomElement([
+                    'Mrs',
+                    'Ms',
+                    ''
+                ]),
+                default  => '',
             };
 
-
+            $rand = rand(0, 100);
+            $status = match (true) {
+                $rand <= 15 => PatientStatus::Inactive,
+                $rand > 15 && $rand <= 25 => PatientStatus::Prospective,
+                default     => PatientStatus::Active,
+            };
             $model = Patient::factory()
-                         ->create([
-                             'prefix'     => $prefix,
-                             'suffix'     => fake()->randomElement(['Jr.', 'Sr.', 'II', 'III', '']),
-                             'first_name' => $first_name,
-                             'middle_name' => count($name) > 0 ? implode(' ', $name) : '',
-                             'last_name'  => $last_name === '' ? 'N/A' : $last_name,
-                             'email'      => $email,
-                             'password'   => bcrypt('password'),
-                             'date_of_birth' => fake()->dateTimeBetween('-100 years', '-1 years'),
-                             'nickname'   => "",
-                             'created_at' => $created_at,
-                             'updated_at' => $created_at,
-                         ]);
+                            ->create([
+                                'prefix'        => $prefix,
+                                'suffix'        => fake()->randomElement([
+                                    'Jr.',
+                                    'Sr.',
+                                    'II',
+                                    'III',
+                                    ''
+                                ]),
+                                'first_name'    => $first_name,
+                                'middle_name'   => count($name) > 0 ? implode(' ', $name) : '',
+                                'last_name'     => $last_name === '' ? 'N/A' : $last_name,
+                                'email'         => $email,
+                                'password'      => bcrypt('password'),
+                                'date_of_birth' => fake()->dateTimeBetween('-100 years', '-1 years'),
+                                'nickname'      => "",
+                                'status'        => $status,
+                                'created_at'    => $created_at,
+                                'updated_at'    => $created_at,
+                            ]);
 
             $avatar_path = database_path('src/avatars/'.str_replace(' ', '-', $character['id']).'.jpeg');
             if (!file_exists($avatar_path)) {
@@ -94,7 +139,7 @@ class PatientSeeder extends Seeder
                 echo $e->getMessage();
             }
 
-            if ($counter++ === 100) {
+            if ($counter++ === 200) {
                 die('Dead');
             }
             $already_in[] = $character['name'];
