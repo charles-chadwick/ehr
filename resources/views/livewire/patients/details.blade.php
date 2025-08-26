330d4b5179818247c0529d078291b14517f33338
charles <charles@mutuallyassured.com>
2025-08-26 14:22:56 -0700

    Styling and adding optional menu.

--- resources/views/livewire/patients/details.blade.php
+++ resources/views/livewire/patients/details.blade.php
@@ -8,31 +8,59 @@ new class extends Component {
     use PatientStatusColor;
 
     public Patient $patient;
-
+    public string  $menu = "";
 }; ?>
 
 <div>
     <div class="flex flex-row text-sm">
-        <div class="flex-none mr-4">
+        <div class="flex-none mr-2">
             <flux:avatar
                     src="{{ $patient->avatar }}"
                     alt=""
-                    class="w-14 h-14"
+                    class="w-16 h-16"
             />
         </div>
         <div class="flex-grow">
-            <p class="font-bold">
-                <a href="{{ route('patients.chart', $patient) }}">
-                    {{ $patient->full_name_extended }}
-                </a>
+            <a
+                    href="{{ route('patients.chart', $patient) }}"
+                    class="font-semibold"
+            >
+                {{ $patient->full_name_extended }}
+            </a>
+            <p>{{ $patient->gender }} / {{ $patient->age }} ({{ $patient->dob }})</p>
+            <p>
+                <flux:badge
+                        class="h-5"
+                        size="sm"
+                        variant="primary"
+                        color="{{ $this->statusColor($patient->status) }}"
+                >
+                    {{ $patient->status }}
+                </flux:badge>
             </p>
-            <p>{{ $patient->age }} ({{ $patient->dob }})</p>
-            <p>{{ $patient->gender }}</p>
         </div>
-        <div class="flex-none">
-            <flux:badge variant="primary" color="{{ $this->statusColor($patient->status) }}">
-                {{ $patient->status }}
-            </flux:badge>
+        <div class="flex-none text-right">
+            @if($menu !== "")
+                <flux:dropdown
+                        position="bottom"
+                        align="end"
+                >
+                    <flux:button
+                            size="sm"
+                            icon="ellipsis-horizontal"
+                            variant="ghost"
+                            inset="top bottom"
+                    ></flux:button>
+                    <flux:navmenu>
+                        <flux:navmenu.item
+                                href="#"
+                                icon="user"
+                        >
+                            {{ __('ehr.edit_profile') }}
+                        </flux:navmenu.item>
+                    </flux:navmenu>
+                </flux:dropdown>
+            @endif
         </div>
     </div>
 </div>
