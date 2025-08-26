d047122f507f1eeeaab38f6d8e41da2b23c8c19d
charles <charles@mutuallyassured.com>
2025-08-26 12:39:52 -0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Livewire\Volt\Volt;
 
 Volt::route('appointments/create', 'appointments.create')->name('appointments.create');
 Volt::route('appointments/{appointment}/view', 'appointments.view')->name('appointments.view');
+Volt::route('patients/{patient}/', 'appointments.index')->name('appointments.index');
