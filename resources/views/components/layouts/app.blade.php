9faee415a00fa8b35188661f828acf56b6c138a4
charles <charles@mutuallyassured.com>
2025-08-26 14:22:43 -0700

    Formatting

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,29 +7,25 @@
             content="width=device-width, initial-scale=1"
     >
 
-    <title>{{ $title ?? config('app.name') }}</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel</title>
+
     <!-- Fonts -->
     <link
             rel="preconnect"
             href="https://fonts.bunny.net"
     >
-    <link
-            href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
-            rel="stylesheet"
-    />
+
     @fluxAppearance
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
-
 </head>
-<body class="h-screen bg-zinc-100 text-zinc-700">
+<body class="h-screen  bg-zinc-100">
 @persist('toast')
 <flux:toast />
 @endpersist
-<div class="max-w-7xl mx-auto m-6 sm:px-6 lg:px-8">
-{{ $slot }}
+<div class="m-6 mx-auto mt-4 sm:px-6 lg:px-8">
+    {{ $slot }}
 </div>
 @fluxScripts
 @livewireScripts
