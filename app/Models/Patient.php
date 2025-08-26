e2667b24183fa9afaa4e0a9157f091b6d82e101c
charles <charles@mutuallyassured.com>
2025-08-26 10:17:10 -0700

    Removed stuff for now

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 
 class Patient extends Base implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
