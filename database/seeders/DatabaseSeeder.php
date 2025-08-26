576e31f61de5c7c6fa1bd5e9dedd43bf5569d231
charles <charles@mutuallyassured.com>
2025-08-26 09:40:37 -0700

    More seeder stuff

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run() : void
     {
-        // (new UserSeeder())->run();
-//        (new PatientSeeder())->run();
+         (new UserSeeder())->run();
+        (new PatientSeeder())->run();
     }
 }
