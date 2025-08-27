5cdab072e61dba61fc5ddc80fd68adae6880b064
charles <charles@mutuallyassured.com>
2025-08-27 16:06:05 -0700

    Language

--- lang/en/encounters.php
+++ lang/en/encounters.php
@@ -1,15 +1,14 @@
 <?php
 
 return [
-    'new_encounter' => 'New Encounter',
-    'encounters' => 'Encounters',
+    'encounter'       => 'Encounter',
+    'encounters'      => 'Encounters',
     'date_of_service' => 'Date of Service',
-    'title' => 'Title',
-    'type' => 'Type',
-    'status' => 'Status',
-    'no_encounters' => 'There are no encounters for this patient.',
-    'my_encounter' => 'My Encounter',
-    'save_and_sign' => 'Save and Sign',
-    'unsign' => 'Unsign',
-    'signed_by' => 'Signed by :name on :date'
+    'title'           => 'Title',
+    'type'            => 'Type',
+    'content'            => 'Content',
+    'status'          => 'Status',
+    'save_and_sign'   => 'Save and Sign',
+    'unsign'          => 'Unsign',
+    'signed_by'       => 'Signed by :name on :date'
 ];
