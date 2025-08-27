e6296a25235dc2b1433887eb6a3e77fb6a5da172
charles <charles@mutuallyassured.com>
2025-08-26 22:17:40 -0700

    Language stuff

--- lang/en/appointments.php
+++ lang/en/appointments.php
@@ -1,13 +1,16 @@
 <?php
 return [
-    'appointment'      => 'Appointment',
-    'date_and_time'     => 'Date and Time',
-    'title'             => 'Title',
-    'type'              => 'Type',
-    'status'            => 'Status',
-    'date'              => 'Date',
-    'time'              => 'Time',
-    'length'            => 'Length',
-    'choose_status'     => 'Choose Status',
-    'in_minutes'        => 'In Minutes',
+    'appointment'   => 'Appointment',
+    'appointments'  => 'Appointments',
+    'date_and_time' => 'Date and Time',
+    'title'         => 'Title',
+    'type'          => 'Type',
+    'status'        => 'Status',
+    'date'          => 'Date',
+    'time'          => 'Time',
+    'length'        => 'Length',
+    'choose_status' => 'Choose Status',
+    'in_minutes'    => 'In Minutes',
+
+    'schedule_new' => 'Schedule New'
 ];
--- lang/en/ehr.php
+++ lang/en/ehr.php
@@ -1,4 +1,9 @@
 <?php
 return [
-    'save' => 'Save',
+    'save'       => 'Save',
+    'cancel'     => 'Cancel',
+    'delete'     => 'Delete',
+    'edit'       => 'Edit',
+    'create_new' => 'Create New :item',
+    'no_records' => 'No :items Found',
 ];
