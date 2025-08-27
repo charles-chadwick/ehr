09e61acbc8840d82ff25ad96c2a1403d8d8a82e9
charles <charles@mutuallyassured.com>
2025-08-26 23:19:40 -0700

    Adjusted user list

--- resources/views/livewire/users/show-list.blade.php
+++ resources/views/livewire/users/show-list.blade.php
@@ -9,16 +9,15 @@ new class extends Component {
 
 }; ?>
 
-<dl class="flex w-full flex-none justify-between gap-x-1 sm:w-auto">
+<dl class="flex w-full flex-none justify-between gap-x sm:w-auto">
     @foreach ($users as $user)
         <dd wire:key="user-{{ $user->id.'-'.uniqid() }}">
             <img
                     src="{{ $user->avatar }}"
-                    alt="Emma Dorsey"
+                    alt="{{ $user->full_name }}"
+                    title="{{ $user->full_name }}"
                     class="size-6 rounded-full bg-gray-50 outline-2 outline-white dark:bg-gray-800 dark:outline-gray-900"
             />
         </dd>
     @endforeach
-
-
 </dl>
