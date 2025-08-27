5323d0237290a9f021cc8816a8320f026ea73d81
charles <charles@mutuallyassured.com>
2025-08-27 15:06:02 -0700

    Logged in via CLI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Activitylog\Facades\CauserResolver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (! $this->app->runningInConsole()) {
+            auth()->loginUsingId(5);
+            CauserResolver::setCauser(auth()->user());
+        }
     }
 }
