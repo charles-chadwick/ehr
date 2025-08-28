49e99d226cef465593eb9dceb1116b139e79e3c5
charles <charles@mutuallyassured.com>
2025-08-28 00:21:29 -0700

    Basic note stuff

---
+++ app/Models/Note.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\NoteType;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Note extends Base
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'notable_id',
+        'notable_type',
+        'type',
+        'title',
+        'content'
+    ];
+
+    public function casts() : array
+    {
+        return [
+            'type' => NoteType::class,
+        ];
+    }
+
+    public function notable() : MorphTo
+    {
+        return $this->morphTo();
+    }
+}
