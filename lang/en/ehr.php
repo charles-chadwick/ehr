3e0efad8449eac72b529cbf2e7f40235b1930677
charles <charles@mutuallyassured.com>
2025-08-28 19:12:41 -0700

    Added a basic search.
    Need to abstract this into a trait

--- lang/en/ehr.php
+++ lang/en/ehr.php
@@ -1,11 +1,12 @@
 <?php
 return [
-    'save'       => 'Save',
-    'cancel'     => 'Cancel',
-    'delete'     => 'Delete',
-    'edit'       => 'Edit :item',
-    'options'    => 'Options',
-    'create_new' => 'Create New :item',
-    'no_records' => 'No :items Found',
+    'search'      => 'Search',
+    'save'        => 'Save',
+    'cancel'      => 'Cancel',
+    'delete'      => 'Delete',
+    'edit'        => 'Edit :item',
+    'options'     => 'Options',
+    'create_new'  => 'Create New :item',
+    'no_records'  => 'No :items Found',
     'upload_file' => 'Upload File',
 ];
--- resources/views/livewire/patients/index.blade.php
+++ resources/views/livewire/patients/index.blade.php
@@ -3,6 +3,7 @@
 
 use App\Livewire\Traits\Sortable;
 use App\Models\Patient;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pagination\LengthAwarePaginator;
 use LaravelIdea\Helper\App\Models\_IH_Base_C;
 use LaravelIdea\Helper\App\Models\_IH_Appointment_C;
@@ -18,10 +19,13 @@ new class extends Component {
 
     use Sortable;
 
+    public string $search_term = "";
+
     public function mount(Patient $patient) : void
     {
         $this->patient = $patient;
         $this->sort_by = 'id';
+        $this->sort_direction = 'asc';
     }
 
     #[Computed]
@@ -29,7 +33,16 @@ new class extends Component {
     public function patients() : array|_IH_Patient_C|LengthAwarePaginator|_IH_Base_C
     {
         return Patient::orderBy($this->sort_by, $this->sort_direction)
+                      ->when($this->search_term !== "", function (Builder $query) {
+                          $query->whereAny(['first_name', 'last_name', 'id'], 'like', trim($this->search_term).'%');
+                      })
                       ->paginate();
+    }
+
+    public function search() : void
+    {
+        $this->resetPage();
+        $this->patients();
     }
 
     public function with() : array
@@ -42,49 +55,52 @@ new class extends Component {
 
 <div>
     {{-- patient modals --}}
-    <flux:button>
-        <flux:modal.trigger name="create-patient">
-            Create new
-        </flux:modal.trigger>
-    </flux:button>
+    <div class="flex justify-between mb-4">
+        <flux:button>
+            <flux:modal.trigger name="create-patient">
+                Create new
+            </flux:modal.trigger>
+        </flux:button>
+        <div class="search-form">
+            <flux:input
+                    icon="magnifying-glass"
+                    wire:model="search_term"
+                    wire:keyup="search"
+                    type="text"
+                    placeholder="{{ __('ehr.search') }}"
+                    class="w-full sm:w-auto"
+            />
+        </div>
+    </div>
     <flux:modal name="create-patient">
         <livewire:patients.create
-                modal="create-patient"
+            modal="create-patient"
         />
     </flux:modal>
-
-    {{-- patient list --}}
-    <ul
+    <flux:card>
+        {{-- patient list --}}
+        <ul
             role="list"
-            class="divide-y divide-gray-100 dark:divide-white/5  text-sm"
-    >
-        @forelse($patients as $patient)
-            <li
+            class="list-group"
+        >
+            @forelse($patients as $patient)
+                <li
                     wire:key="patient-{{ $patient->id }}"
-                    class="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
-            >
-                <div>
-                    <p class="font-semibold">
-                        <a
-                                href="#"
-                                class="link font-bold"
-                        >
-                            <a
-                                    class="link"
-                                    href="{{ route('patients.chart', $patient) }}"
-                            >{{ $patient->full_name }}</a>
-                        </a>
-                    </p>
-                    <div class="mt-1 flex items-center gap-x-2">
-                        <p>
-                            {{ $patient->dob }}
-                        </p>
-                    </div>
-                </div>
+                    class="list-item"
+                >
+                    <livewire:patients.details
+                        :patient="$patient"
+                        :short_name="true"
+                        wire:key="patient-{{ $patient->id }}-details"
+                    />
+                </li>
+            @empty
+                <li class="text-center">{{ __('ehr.no_records', ['items' => __('patients.patients')]) }}</li>
+            @endforelse
+        </ul>
+        <div class="my-4">
+            {{ $patients->links() }}
+        </div>
+    </flux:card>
 
-            </li>
-        @empty
-            <li class="text-center">{{ __('ehr.no_records', ['items' => __('patients.patients')]) }}</li>
-        @endforelse
-    </ul>
 </div>
