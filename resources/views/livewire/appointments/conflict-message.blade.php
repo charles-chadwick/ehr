c733d55a00231d1a55e36a4b4cdf2232922dff1f
charles <charles@mutuallyassured.com>
2025-08-29 14:05:03 -0700

    Refactored to use Flux callout.

--- resources/views/livewire/appointments/conflict-message.blade.php
+++ resources/views/livewire/appointments/conflict-message.blade.php
@@ -1,8 +1,14 @@
-<div class="text-xs p-2 m-2 text-white bg-red-700 rounded-sm">
-    <p class="font-bold">The following users already have appointments at this time:</p>
-    <ul role="list">
-    @foreach($users as $user)
-        <li>{{ $user->full_name_extended }}</li>
-    @endforeach
-    </ul>
-</div>
+<flux:callout
+        class="mb-4"
+        heading="{{ __('appointments.schedule_conflict') }}"
+        icon="exclamation-circle"
+        variant="danger"
+>
+    <flux:callout.text>
+        <ul role="list">
+            @foreach($users as $user)
+                <li>{{ $user->full_name_extended }}</li>
+        @endforeach
+
+    </flux:callout.text>
+</flux:callout>
