59c9b69da2d703ba1353ef41a37757ca0118fa2b
charles <charles@mutuallyassured.com>
2025-08-29 14:54:41 -0700

    Fixed language issue

--- lang/en/ehr.php
+++ lang/en/ehr.php
@@ -14,8 +14,8 @@ return [
     // Success Messages
     'success_heading' => 'Success!',
     'success_created' => 'Successfully created :item.',
-    'success_updated' => 'Successfully updating :item.',
-    'success_deleted' => 'Successfully deleting :item.',
+    'success_updated' => 'Successfully updated :item.',
+    'success_deleted' => 'Successfully deleted :item.',
 
     'error_heading' => 'Error!',
     'error_creating' => 'Error creating :item.',
