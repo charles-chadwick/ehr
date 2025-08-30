94f2fd219de2fd81b49a17875c4f7a6bebc5ab9a
charles <charles@mutuallyassured.com>
2025-08-30 00:26:29 -0700

    Language

--- lang/en/patients.php
+++ lang/en/patients.php
@@ -1,20 +1,25 @@
 <?php
 
 return [
-    'patient' => 'Patient',
+    'patient'  => 'Patient',
     'patients' => 'Patients',
-    'prefix' => 'Prefix',
-    'status' => 'Status',
-    'first_name' => 'First Name',
-    'middle_name' => 'Middle Name',
-    'last_name' => 'Last Name',
-    'suffix' => 'Suffix',
-    'nickname' => 'Nickname',
-    'date_of_birth' => 'Date of Birth',
-    'gender' => 'Gender',
-    'gender_identity' => 'Gender Identity',
-    'email' => 'Email',
-    'password' => 'Password',
+
+    /**
+     * Fields
+     */
+    'mrn'              => 'MRN',
+    'prefix'           => 'Prefix',
+    'status'           => 'Status',
+    'first_name'       => 'First Name',
+    'middle_name'      => 'Middle Name',
+    'last_name'        => 'Last Name',
+    'suffix'           => 'Suffix',
+    'nickname'         => 'Nickname',
+    'date_of_birth'    => 'Date of Birth',
+    'gender'           => 'Gender',
+    'gender_identity'  => 'Gender Identity',
+    'email'            => 'Email',
+    'password'         => 'Password',
     'confirm_password' => 'Confirm Password',
 
 ];
